32fadc485278cb81a15ce3f2a5f8521a459661f4
Nor4aa <91344757+Nor4aa@users.noreply.github.com>
2021-09-26 08:34:46 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,140 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.4 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+/* Description: Custom JS file */
 
-window.addEventListener('DOMContentLoaded', event => {
+/* Navigation*/
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+window.onload = function () {
+	scrollFunction();
+};
 
-    };
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 30) {
+		document.getElementById("navbarExample").classList.add("top-nav-collapse");
+	} else if ( document.documentElement.scrollTop < 30 ) {
+		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+	}
+}
 
-    // Shrink the navbar 
-    navbarShrink();
+// Navbar on mobile
+let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+for (let i = 0; i < elements.length; i++) {
+	elements[i].addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+	});
+}
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
 });
+
+// Hover on desktop
+function toggleDropdown(e) {
+	const _d = e.target.closest(".dropdown");
+	let _m = document.querySelector(".dropdown-menu", _d);
+
+	setTimeout(
+		function () {
+		const shouldOpen = _d.matches(":hover");
+		_m.classList.toggle("show", shouldOpen);
+		_d.classList.toggle("show", shouldOpen);
+
+		_d.setAttribute("aria-expanded", shouldOpen);
+		},
+		e.type === "mouseleave" ? 300 : 0
+	);
+}
+
+// On hover
+const dropdownCheck = document.querySelector('.dropdown');
+
+if (dropdownCheck !== null) { 
+	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+
+	// On click
+	document.querySelector(".dropdown").addEventListener("click", (e) => {
+		const _d = e.target.closest(".dropdown");
+		let _m = document.querySelector(".dropdown-menu", _d);
+		if (_d.classList.contains("show")) {
+			_m.classList.remove("show");
+			_d.classList.remove("show");
+		} else {
+			_m.classList.add("show");
+			_d.classList.add("show");
+		}
+	});
+}
+  
+
+/* Card Slider - Swiper */
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
+
+
+/* Image Slider - Swiper */
+var imageSlider = new Swiper('.image-slider', {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	spaceBetween: 50,
+	slidesPerView: 5,
+	breakpoints: {
+		// when window is <= 575px
+		575: {
+			slidesPerView: 1,
+			spaceBetween: 10
+		},
+		// when window is <= 767px
+		767: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		// when window is <= 991px
+		991: {
+			slidesPerView: 3,
+			spaceBetween: 20
+		},
+		// when window is <= 1199px
+		1199: {
+			slidesPerView: 4,
+			spaceBetween: 20
+		},
+
+	}
+});
+
+
+/* Back To Top Button */
+// Get the button
+myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // for Safari
+	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+}
